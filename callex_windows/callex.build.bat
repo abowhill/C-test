6b039b67f3d90a11b6ca42a2d92ce3fdb0f2846b
abowhill <ajcbowhill@gmail.com>
2015-06-16 17:08:25 -0700

    Second Commit

--- callex_windows/callex.build.bat
+++ callex_windows/callex.build.bat
@@ -4,7 +4,8 @@ call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat"
 
 REM compiler flags must not have -WX ar -W3
 REM /EHsc must be added to end
-set CommonCompilerFlags=-O2 -MTd -nologo -fp:fast -fp:except- -Gm- -GR- -EHa- -Zo -Oi -W3 -wd4201 -wd4100 -wd4189 -wd4505 -wd4127 -DHANDMADE_INTERNAL=1 -DHANDMADE_SLOW=1 -DHANDMADE_WIN32=1 -FC -Z7 /EHsc
+REM Based on Casey Muratori's Handmade Hero build file 
+set CommonCompilerFlags=-O2 -MTd -nologo -fp:fast -fp:except- -Gm- -GR- -EHa- -Zo -Oi -W3 -wd4201 -wd4100 -wd4189 -wd4505 -wd4127 -FC -Z7 /EHsc
 
 REM call with flags, errors to text file
 cl.exe %CommonCompilerFlags% callex.cpp > compiler_err.txt 2>&1
